e6ccd79883797d28a25447f7be6484ccf77d2599
Matt Beanland <beanland@live.com.au>
2025-02-24 00:28:03 +1100

    more tests

---
+++ test/list_of_map_test.exs
@@ -0,0 +1,15 @@
+defmodule Outstanding.ListOfMapTest do
+  use ExUnit.Case
+  use Outstand
+
+  gen_something_outstanding_test("element outstanding", [%{a: "a"}, %{b: "b"}], [%{b: "b"}, %{c: "c"}])
+  gen_something_outstanding_test("order outstanding", [%{a: "a"}, %{b: "b"}], [%{b: "b"}, %{a: "a"}])
+  gen_something_outstanding_test("empty outstanding", [], [%{a: "a"}])
+  gen_something_outstanding_test("extra outstanding", [%{a: "a"}, %{b: "b"}], [%{a: "a"}, %{b: "b"}, %{c: "c"}])
+  gen_nothing_outstanding_test("realized", [%{a: "a"}, %{b: "b"}], [%{a: "a"}, %{b: "b"}])
+  gen_nothing_outstanding_test("realised, extra elements in map", [%{a: "a"}, %{b: "b"}], [%{a: "a", c: "c"}, %{b: "b", d: "d"}])
+  gen_result_outstanding_test("element result", [%{a: "a"}, %{b: "b"}], [%{b: "b"}, %{c: "c"}], [%{a: "a"}, %{b: "b"}])
+  gen_result_outstanding_test("order result", [%{a: "a"}, %{b: "b"}], [%{b: "b"}, %{a: "a"}], [%{a: "a"}, %{b: "b"}])
+  gen_result_outstanding_test("empty result", [], [%{a: "a"}], [])
+  gen_result_outstanding_test("extra result", [%{a: "a"}, %{b: "b"}], [%{a: "a"}, %{b: "b"}, %{c: "c"}], [%{a: "a"}, %{b: "b"}])
+end
--- test/map_test.exs
+++ test/map_test.exs
@@ -4,8 +4,8 @@ defmodule Outstanding.MapTest do
 
   gen_something_outstanding_test("key outstanding", %{x: :a, y: :b}, %{x: :a, z: :b})
   gen_something_outstanding_test("value outstanding", %{x: :a, y: :b}, %{x: :b, y: :b})
-  gen_nothing_outstanding_test("realizes", %{x: :a, y: :b}, %{x: :a, y: :b})
-  gen_nothing_outstanding_test("realizes with extra item", %{x: :a, y: :b}, %{x: :a, y: :b, z: :b})
+  gen_nothing_outstanding_test("realized", %{x: :a, y: :b}, %{x: :a, y: :b})
+  gen_nothing_outstanding_test("realized with extra item", %{x: :a, y: :b}, %{x: :a, y: :b, z: :b})
   gen_result_outstanding_test("key result", %{x: :a, y: :b}, %{x: :a, z: :b}, %{y: :b})
   gen_result_outstanding_test("value result", %{x: :a, y: :b}, %{x: :b, y: :b},  %{x: :a})
 end
