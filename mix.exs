0e0338061108800db55abba1403cfec420247e3c
Matt Beanland <beanland@live.com.au>
2025-05-12 23:21:17 +1000

    livebook

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Outstanding.MixProject do
   def project do
     [
       app: :outstanding,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       consolidate_protocols: Mix.env() != :test,
       start_permanent: Mix.env() == :prod,
