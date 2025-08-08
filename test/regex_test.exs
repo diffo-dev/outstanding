4ef32a098a02d3cae855c18b745fe50e977545d3
Matt Beanland <beanland@live.com.au>
2025-08-08 20:59:04 +1000

    fixed

--- test/regex_test.exs
+++ test/regex_test.exs
@@ -6,5 +6,11 @@ defmodule Outstanding.RegexTest do
   gen_nothing_outstanding_test("realized", ~r/foo/, "foo")
   gen_nothing_outstanding_test("realized, match within string", ~r/foo/, "barfoobar")
   gen_nothing_outstanding_test("realized, match within String.Chars implementation", ~r/foo/, :barfoobar)
-  gen_result_outstanding_test("value result", ~r/foo/, "bar", ~r/foo/)
+  #doesn't work as ~r/foo/ gets compiled separately so doesn't match
+  #gen_result_outstanding_test("value result", ~r/foo/, "bar", ~r/foo/)
+  test "value result" do
+    foo_regex = ~r/foo/
+    outstanding = foo_regex --- "bar"
+    assert outstanding == foo_regex
+  end
 end
